bf27b523ac4e198f24f50b39131d480bc9ad2e5a
Diegodsito <143525921+Diegodsito@users.noreply.github.com>
2024-08-27 01:20:41 +0000

    Implementación base de menú
    Se implementa menú de opciones básico.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,44 @@
 import java.io.*;
 
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+  public static void main(String[] args)throws IOException {
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
-}
+    String ingresado;
+    int opcion;
+
+    BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
+
+    do{
+
+      System.out.println("Bienvenido al sistema de venta de entradas");
+      System.out.println("1. Eventos disponibles");
+      System.out.println("2. Buscar evento por ID");
+      System.out.println("3. Cancelar compra");
+      System.out.println("4. Salir");
+
+      ingresado = lector.readLine();
+      opcion = Integer.parseInt(ingresado);
+
+      switch(opcion){
+        case 1:
+          //mostrarEventosDisponible();
+          break;
+        case 2:
+          //buscarEventoPorID(Id);
+          break;
+        case 3:
+          //cancelarCompra(Id);
+          break;
+        case 4:
+          System.out.println("Gracias por usar el sistema de venta de entradas");
+          break;
+        default:
+          System.out.println("Opcion no valida");
+          break;
+      }
+      
+      
+    } while(opcion != 4);
+
+    
+  }
